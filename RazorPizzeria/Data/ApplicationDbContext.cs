8a585160ee23a79f3005cf1a0561ca8f5eeea077
wince <wince@DESKTOP-OQC82C7>
2023-03-07 12:23:25 +0100

    Dodaj pliki projektów.

---
+++ RazorPizzeria/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPizzeria.Models;
+
+namespace RazorPizzeria.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<PizzasOrder> PizzasOrders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
